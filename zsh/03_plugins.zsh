ddd12c5e3eeb7c305a04a0e76350ff67831766b1
飯田惟吹 <ibuki.iida@topgate.co.jp>
2022-02-06 19:27:56 +0900

    fix plugins

--- zsh/03_plugins.zsh
+++ zsh/03_plugins.zsh
@@ -7,12 +7,7 @@
 # ---------------------------------------------------------
 
 
-# Load a few important annexes, without Turbo
-# (this is currently required for annexes)
-zinit light-mode for \
-    zinit-zsh/z-a-patch-dl \
-    zinit-zsh/z-a-as-monitor \
-    zinit-zsh/z-a-bin-gem-node
+
 
 
 
